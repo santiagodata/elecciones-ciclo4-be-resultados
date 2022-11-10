d6a19c2058a6ada0f611f9d3ed93a3a51a3cfad4
Santiago Rodriguez Arroyave <75347201+santiagodata@users.noreply.github.com>
2022-11-10 12:24:57 -0500

    Delete RepositorioEstudiante.py

--- Repositorios/RepositorioEstudiante.py
+++
@@ -1,5 +0,0 @@
-from Repositorios.InterfaceRepositorio import InterfaceRepositorio
-from Modelos.Estudiante import Estudiante
-class RepositorioEstudiante(InterfaceRepositorio[Estudiante]):
-    pass
-
